a3c445cb3dd96656471ab9e22c53b3908f2eb9f8
Quentin Pitteloud <quentin.pitteloud@uit.no>
2026-04-17 17:33:25 +0200

    added deep copy to apply_pauli and maybe a better design to apply_pauli: apply_alpha. Could also add provisions to apply_pauli in case the input and output are the same.

--- src/utils/spinor_utils.cpp
+++ src/utils/spinor_utils.cpp
@@ -6,49 +6,45 @@
 
 #include <complex>
 #include <iostream>
+
+// remove below
+#include "Printer.h"
+// till here
+
 using namespace std::complex_literals;
 
 namespace mrcpp {
-    
-    // /*
-    //  * @brief: shuffles the indices of a spinor, simulating the application of a Dirac matrix to it
-    //  * pauli represents the index of the Dirac matrices.
-    //  * For scalar operators, it is unused.
-    //  * For 2 component (Weyl/Pauli) spinors, pauli = 0,1,2,3 corresponds to indentiy, sigma_x, y and z respectively.
-    //  * 
-    // */
-    void apply_Pauli(CompFunction<3> &out, const CompFunction<3> &inp, int pauli, double prec, bool conjugate) { //NOTE: assumes 2-component spinors for now
-        // Implementation of applying Pauli matrices to spinor functions
-        // This function will modify 'out' based on the Pauli matrix specified by 'pauli'
+
+    CompFunction<3> apply_alpha(const CompFunction<3> &inp, int alpha, bool conjugate) {
+        // Implementation of applying Alpha matrices to spinor functions
+        // This function will modify 'out' based on the Alpha matrix specified by 'Alpha'
         // and the input function 'inp'.
         // The 'prec' parameter is used for precision control.
         // The 'conjugate' parameter indicates whether to apply conjugation.
         ComplexDouble comp_i(0.0, 1.0); // Define the imaginary unit
-        switch (pauli) {
+        CompFunction<3> out(inp);
+        switch (alpha) {
         case 0:
-            //Identity, base case, nothing to apply, just copy the input to the output if they are not the same function
-            if (&out != &inp) {
-                out = inp;
-            }
+            //Identity, base case, nothing to apply
             break;
         case 1:
-            // Apply Pauli-X matrix
+            // Apply Alpha-X matrix
             for (int i = 0; i < inp.Ncomp(); i = i + 2) {
                 if (inp.isreal() == 1) {
                     out.defreal(); // Safety catch for later operations on out. If the input is real, the output should also be defined as real
                     // Warning: shallow copy
-                    // out.CompD[i] = inp.CompD[i+1];
-                    // out.CompD[i+1] = inp.CompD[i];
-                    out.setReal(inp.CompD[i+1], i);
-                    out.setReal(inp.CompD[i], i+1);
+                    out.CompD[i] = inp.CompD[i+1];
+                    out.CompD[i+1] = inp.CompD[i];
+                    // out.setReal(inp.CompD[i+1], i);
+                    // out.setReal(inp.CompD[i], i+1);
 
                 } else {
                     out.defcomplex(); // Safety catch for later operations on out. If the input is complex, the output should also be defined as complex
                     // Warning: shallow copy
-                    // out.CompC[i] = inp.CompC[i+1];
-                    // out.CompC[i+1] = inp.CompC[i];
-                    out.setCplx(inp.CompC[i+1], i);
-                    out.setCplx(inp.CompC[i], i+1);
+                    out.CompC[i] = inp.CompC[i+1];
+                    out.CompC[i+1] = inp.CompC[i];
+                    // out.setCplx(inp.CompC[i+1], i);
+                    // out.setCplx(inp.CompC[i], i+1);
                 }
                 // coefficient multiplication needn't be done separately for real and complex cases, since the coefficient is purely imaginary, so it will just be multiplied to the complex part of the function, even if the function is defined as real. However, we need to make sure that the output function is defined as complex in this case, otherwise we might run into issues later on when trying to multiply it by a complex coefficient. 
                 out.func_ptr->data.c1[i] = inp.func_ptr->data.c1[i+1];
@@ -56,10 +52,10 @@ namespace mrcpp {
             }
             break;
         case 2:
-            // Apply Pauli-Y matrix
-            // std::cout << "apply Pauli Y tut0 " << out.Ncomp() << " " << inp.Ncomp() << '\n';
+            // Apply Alpha-Y matrix
+            // std::cout << "apply Alpha Y tut0 " << out.Ncomp() << " " << inp.Ncomp() << '\n';
             for (int i = 0; i < inp.Ncomp(); i = i + 2) {
-                // std::cout << "Applying Pauli-Y matrix " << i << " " << inp.isreal() << std::endl;
+                // std::cout << "Applying Alpha-Y matrix " << i << " " << inp.isreal() << std::endl;
                 if (inp.isreal() == 1) {
                     out.defreal(); // Safety catch for later operations on out. If the input is real, the output should also be defined as real
                     // Warning: shallow copy
@@ -82,7 +78,7 @@ namespace mrcpp {
             }
             break;
         case 3:
-            // Apply Pauli-Z matrix
+            // Apply Alpha-Z matrix
             for (int i = 0; i < inp.Ncomp(); i = i + 2) {
                 if (inp.isreal() == 1) {
                     out.defreal(); // Safety catch for later operations on out. If the input is real, the output should also be defined as real
@@ -97,6 +93,136 @@ namespace mrcpp {
                     // Warning: shallow copy
                     out.setCplx(inp.CompC[i], i); 
                     out.setCplx(inp.CompC[i+1], i+1); 
+                    // out.CompC[i] = inp.CompC[i+1]; 
+                    // out.CompC[i+1] = inp.CompC[i];
+                }
+                out.func_ptr->data.c1[i] = inp.func_ptr->data.c1[i];
+                // out.CompC[i+1] = inp.CompC[i+1]; 
+                // out.CompC[i+1]->rescale(-1.0);
+                out.func_ptr->data.c1[i+1] = inp.func_ptr->data.c1[i+1] * (-1.0);
+                // out.func_ptr->data.c1[i+1] *= -1.0;
+            }
+            break;
+        default:
+            std::cerr << "Invalid Alpha matrix index, values must be 0,1,2,3. Current value: " << alpha << std::endl;
+        }
+        return out;
+    }
+    
+    // /*
+    //  * @brief: shuffles the indices of a spinor, simulating the application of a Dirac matrix to it
+    //  * pauli represents the index of the Dirac matrices.
+    //  * For scalar operators, it is unused.
+    //  * For 2 component (Weyl/Pauli) spinors, pauli = 0,1,2,3 corresponds to indentiy, sigma_x, y and z respectively.
+    //  * 
+    // */
+    //TODO: changer ça pour que ça retourne un CompFunction et retier out? Ou faire une provision si out et inp sont identiques?
+    void apply_Pauli(CompFunction<3> &out, CompFunction<3> &inp, int pauli, double prec, bool conjugate) { //NOTE: assumes 2-component spinors for now
+        // Implementation of applying Pauli matrices to spinor functions
+        // This function will modify 'out' based on the Pauli matrix specified by 'pauli'
+        // and the input function 'inp'.
+        // The 'prec' parameter is used for precision control.
+        // The 'conjugate' parameter indicates whether to apply conjugation.
+        ComplexDouble comp_i(0.0, 1.0); // Define the imaginary unit
+        // MSG_INFO("ntm pauli de merde " << pauli);
+        switch (pauli) {
+        case 0:
+            //Identity, base case, nothing to apply, just copy the input to the output if they are not the same function
+            if (&out != &inp) {
+                // out.deep_copy(inp);
+                out = inp;
+            }
+            break;
+        case 1:
+            // MSG_INFO("case X "<< inp.Ncomp() << " " << out.Ncomp());
+            // Apply Pauli-X matrix
+            for (int i = 0; i < inp.Ncomp(); i = i + 2) {
+                if (inp.isreal() == 1) {
+                    out.defreal(); // Safety catch for later operations on out. If the input is real, the output should also be defined as real
+                    out.func_ptr->data.iscomplex = 0;
+                    // Warning: shallow copy
+                    // out.CompD[i] = inp.CompD[i+1];
+                    // out.CompD[i+1] = inp.CompD[i];
+                    // out.setReal(new FunctionTree<3, double> (*inp.CompD[i+1]), i);
+                    // out.setReal(new FunctionTree<3, double> (*inp.CompD[i]), i+1);
+                    // MSG_INFO(i << " comp addresses input" << &inp.CompD[i] << " " << &inp.CompD[i+1] << " comp addresses output" << &out.CompD[i] << " " << &out.CompD[i+1]);
+                    inp.CompD[i]->deep_copy(out.CompD[i+1]);
+                    inp.CompD[i+1]->deep_copy(out.CompD[i]);
+                    // out.setReal(inp.CompD[i+1], i);
+                    // out.setReal(inp.CompD[i], i+1);
+
+                } else {
+                    out.defcomplex(); // Safety catch for later operations on out. If the input is complex, the output should also be defined as complex
+                    out.func_ptr->data.isreal = 0;
+                    // Warning: shallow copy
+                    // out.CompC[i] = inp.CompC[i+1];
+                    // out.CompC[i+1] = inp.CompC[i];
+                    inp.CompC[i]->deep_copy(out.CompC[i+1]);
+                    inp.CompC[i+1]->deep_copy(out.CompC[i]);
+                    // out.setCplx(inp.CompC[i+1], i);
+                    // out.setCplx(inp.CompC[i], i+1);
+                }
+                // coefficient multiplication needn't be done separately for real and complex cases, since the coefficient is purely imaginary, so it will just be multiplied to the complex part of the function, even if the function is defined as real. However, we need to make sure that the output function is defined as complex in this case, otherwise we might run into issues later on when trying to multiply it by a complex coefficient. 
+                out.func_ptr->data.c1[i] = inp.func_ptr->data.c1[i+1];
+                out.func_ptr->data.c1[i+1] = inp.func_ptr->data.c1[i];
+            }
+            break;
+        case 2:
+            // MSG_INFO("Y ");
+            // Apply Pauli-Y matrix
+            // std::cout << "apply Pauli Y tut0 " << out.Ncomp() << " " << inp.Ncomp() << '\n';
+            for (int i = 0; i < inp.Ncomp(); i = i + 2) {
+                // std::cout << "Applying Pauli-Y matrix " << i << " " << inp.isreal() << std::endl;
+                if (inp.isreal() == 1) {
+                    out.defreal(); // Safety catch for later operations on out. If the input is real, the output should also be defined as real
+                    out.func_ptr->data.iscomplex = 0;
+                    // Warning: shallow copy
+                    // out.setReal(inp.CompD[i+1], i);
+                    // out.setReal(inp.CompD[i], i+1);
+                    inp.CompD[i]->deep_copy(out.CompD[i+1]);
+                    inp.CompD[i+1]->deep_copy(out.CompD[i]);
+                    // out.CompD[i] = inp.CompD[i+1];
+                    // out.CompD[i+1] = inp.CompD[i];
+
+                } else {
+                    out.defcomplex(); // Safety catch for later operations on out. If the input is complex, the output should also be defined as complex
+                    out.func_ptr->data.isreal = 0;
+                    // Warning: shallow copy
+                    // out.setCplx(inp.CompC[i+1], i);
+                    // out.setCplx(inp.CompC[i], i+1);
+                    inp.CompC[i]->deep_copy(out.CompC[i+1]);
+                    inp.CompC[i+1]->deep_copy(out.CompC[i]);
+                    // out.CompC[i] = inp.CompC[i+1];
+                    // out.CompC[i+1] = inp.CompC[i];
+                }
+                // coefficient multiplication needn't be done separately for real and complex cases, since the coefficient is purely imaginary, so it will just be multiplied to the complex part of the function, even if the function is defined as real. However, we need to make sure that the output function is defined as complex in this case, otherwise we might run into issues later on when trying to multiply it by a complex coefficient.
+                out.func_ptr->data.c1[i] = inp.func_ptr->data.c1[i+1] * (-1.0)*comp_i;
+                out.func_ptr->data.c1[i+1] = inp.func_ptr->data.c1[i] * comp_i;
+            }
+            break;
+        case 3:
+            MSG_INFO("Z ");
+            // Apply Pauli-Z matrix
+            for (int i = 0; i < inp.Ncomp(); i = i + 2) {
+                if (inp.isreal() == 1) {
+                    out.defreal(); // Safety catch for later operations on out. If the input is real, the output should also be defined as real
+                    out.func_ptr->data.iscomplex = 0;
+                    // Warning: shallow copy
+                    // out.setReal(inp.CompD[i], i); 
+                    // out.setReal(inp.CompD[i+1], i+1); 
+                    inp.CompD[i]->deep_copy(out.CompD[i]);
+                    inp.CompD[i+1]->deep_copy(out.CompD[i+1]);
+                    // out.CompD[i] = inp.CompD[i+1];
+                    // out.CompD[i+1] = inp.CompD[i];
+
+                } else {
+                    out.defcomplex(); // Safety catch for later operations on out. If the input is complex, the output should also be defined as complex
+                    out.func_ptr->data.isreal = 0;
+                    // Warning: shallow copy
+                    // out.setCplx(inp.CompC[i], i); 
+                    // out.setCplx(inp.CompC[i+1], i+1); 
+                    inp.CompC[i]->deep_copy(out.CompC[i]);
+                    inp.CompC[i+1]->deep_copy(out.CompC[i+1]);
                     // out.CompC[i] = inp.CompC[i+1]; 
                     // out.CompC[i+1] = inp.CompC[i];
                 }
--- src/utils/spinor_utils.h
+++ src/utils/spinor_utils.h
@@ -6,9 +6,12 @@
 // using namespace std::complex_literals;
 
 namespace mrcpp {
-    //WARNING: These functions assume that the input functions are spinors, i.e. they have either 2 or 4 components and are complex valued.
+    //NOTE: These functions assume that the input functions are spinors, i.e. they have either 2 or 4 components and are complex valued.
 
-    void apply_Pauli(CompFunction<3> &out, const CompFunction<3> &inp, int pauli, double prec = -1.0, bool conjugate = false);
+    CompFunction<3> apply_alpha(const CompFunction<3> &inp, int pauli, bool conjugate = false);
+
+    // orginial implementation of apply_alpha, may be more efficient because everything is done by reference, but it doesn't work in mrchem
+    void apply_Pauli(CompFunction<3> &out, CompFunction<3> &inp, int pauli, double prec = -1.0, bool conjugate = false);
 
     // void dot_spinor(CompFunction<3> &out, const CompFunction<3> &inp_a, const CompFunction<3> &inp_b, double prec = -1.0, bool conjugate = false);
 
@@ -19,7 +22,8 @@ namespace mrcpp {
      * The function is normalized by rescaling each component by the same factor, i.e. Psi_i -> Psi_i / ||Psi||. 
      * @param inp The input spinor function to be normalized. It is modified in place.
      * @param prec The precision for the normalization. If the norm is smaller than prec, the norm is considered to be zero.
-     * NOTE: This function rescales the components of the input function rather as well as 
+     * NOTE: This function rescales the components of the input function rather than its overall scaling coefficient.
+     *       This makes it slower but allows to reset the scaling coefficient to a reasonable value.
      */
     void normalize_spinor(CompFunction<3> &inp, double prec = -1.0);
 
