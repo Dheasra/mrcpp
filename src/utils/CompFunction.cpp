0255a6209fa5744bcdaeff5c7a28641f424038cd
Quentin Pitteloud <quentin.pitteloud@uit.no>
2026-05-16 14:05:49 +0200

    Added conditional allocation in project_real/cplx legacy(?) functions to avoid seg faults if used with classes that don't allocate components in their constructors.

--- src/utils/CompFunction.cpp
+++ src/utils/CompFunction.cpp
@@ -1244,6 +1244,7 @@ void project_real(CompFunction<3> &out, std::function<double(const Coord<3> &r)>
     out.func_ptr->isreal = 1;
     out.func_ptr->iscomplex = 0;
     if (out.Ncomp() < 1) out.alloc(1);
+    for (int comp=0; comp < out.Ncomp(); comp++) if(out.CompD[comp]==nullptr) out.alloc_comp(comp, true);
     if (need_to_project) mrcpp::project<3>(prec, *out.CompD[0], f);
     mpi::share_function(out, 0, 123123, mpi::comm_share);
 }
@@ -1287,6 +1288,7 @@ void project_cplx(CompFunction<3> &out, std::function<ComplexDouble(const Coord<
     out.func_ptr->isreal = 0;
     out.func_ptr->iscomplex = 1;
     if (out.Ncomp() < 1) out.alloc(1);
+    for (int comp=0; comp < out.Ncomp(); comp++) if(out.CompC[comp]==nullptr) out.alloc_comp(comp, true);
     if (need_to_project) mrcpp::project<3>(prec, *out.CompC[0], f);
     mpi::share_function(out, 0, 123123, mpi::comm_share);
 }
