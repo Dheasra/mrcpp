ee54abb0c741c65cccc1afdd0582099b5d0a1fb6
Quentin Pitteloud <quentin.pitteloud@uit.no>
2026-05-27 17:43:18 +0200

    Fixed typos in previous fixes of inplace add. I shouldn't have been so eager in pushing to git

--- src/utils/CompFunction.cpp
+++ src/utils/CompFunction.cpp
@@ -461,7 +461,7 @@ template <int D> void CompFunction<D>::add(ComplexDouble c, CompFunction<D> inp)
     //check if the prefactors are zero, in which case,
     //delete tree, reallocate to zero and reset prefactor to one
     for (int i = 0; i < inp.Ncomp(); i++) {
-        if (std::abs((*this).func_ptr->data.c1[i].real)<MachineZero and std::abs((*this).func_ptr->data.c1[i]).imag<MachineZero) {
+        if (std::abs((*this).func_ptr->data.c1[i].real())<MachineZero and std::abs((*this).func_ptr->data.c1[i].imag()) < MachineZero) {
             this->alloc_comp(i, true);
             (*this).func_ptr->data.c1[i] = {1.0,0.0};
         }
